9ba76d666ad3a3e0f64ab3d11e3872da47c98acc
Lijin Daniel Joseph <lijindanieljoseph@gmail.com>
2020-04-04 20:56:30 +0530

    use.c

---
+++ user.c
@@ -0,0 +1,20 @@
+/*
+ ============================================================================
+ Name        : user.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	int a;
+	printf("ENTER A NUMBER:\n"); /* prints !!!Hello World!!! */
+	scanf("%d",&a);
+	printf("YOU ENTERED NUMBER IS %d",a);
+	return EXIT_SUCCESS;
+}
